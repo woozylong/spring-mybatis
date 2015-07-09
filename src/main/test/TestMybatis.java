66b4a11dde1850635f23d7254e8faa0cf6ec84a2
woozylong <woozylong@gmail.com>
2015-07-09 22:42:12 +0800

    commit

--- src/main/test/TestMybatis.java
+++ src/main/test/TestMybatis.java
@@ -1,7 +1,10 @@
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,8 +16,15 @@ import com.youyou.service.IUserService;
 @ContextConfiguration(locations = { "classpath:spring-mybatis.xml" })
 public class TestMybatis {
 
+//	private ApplicationContext ac = null;
+
 	@Resource
 	private IUserService userService = null;
+
+//	@Before
+//	public void before() {
+//		ac = new ClassPathXmlApplicationContext("spring-mybatis.xml");
+//	}
 
 	@Test
 	public void test1() {
